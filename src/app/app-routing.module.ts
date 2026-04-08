e5f0e9887350a4aa853668f035372bb0f6f30525
Cedric <Cedric>
2026-04-08 18:48:07 +0200

    new push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcceuilComponent } from './acceuil/acceuil.component';
 import { EntrepriseComponent } from './entreprise/entreprise.component';
-import { ServComponent } from './serv/serv.component';
-import { AboutComponent } from './about/about.component';
+
+
 
 const routes: Routes = [
    { path: '', redirectTo: 'acceuil', pathMatch: 'full' },
    { path: 'acceuil', component: AcceuilComponent },
    {path:'entreprise', component: EntrepriseComponent},
-   {path:'serv', component:ServComponent},
-   {path:'about', component:AboutComponent}
+   
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,17 @@ import { AcceuilComponent } from './acceuil/acceuil.component';
 import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { EntrepriseComponent } from './entreprise/entreprise.component';
-import { ServComponent } from './serv/serv.component';
+
 // import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AboutComponent } from './about/about.component'
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AcceuilComponent,
     EntrepriseComponent,
-    ServComponent,
-    AboutComponent
+  
     
   ],
   imports: [
