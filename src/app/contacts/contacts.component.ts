b70bd896cd5b6d4bad58f67fd40a02567acdc247
Cedric <Cedric>
2026-04-20 12:11:11 +0200

    push

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent {
+// sidebarOpen = false;
 
+// toggleSidebar(){
+// this.sidebarOpen = !this.sidebarOpen;
+// }
 }
